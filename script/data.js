3e519f7d188031337189e9d86933a2ab396216e4
Nishad <mahmudnishad253@gmail.com>
2025-03-13 19:15:18 +0600

    error screen

--- script/data.js
+++ script/data.js
@@ -24,9 +24,23 @@ const loadPets = (id) => {
 };
 
 function showPets(pets) {
+  console.log(pets.length);
   const petContainer = document.getElementById("pets");
+  const error = document.getElementById("errorScreen");
   petContainer.innerHTML = ``;
+  if (pets.length == 0) {
+    error.innerHTML = `
+        <img src="../images/error.webp" class="w-32">
+            <h3 class="font-bold text-2xl">No Information Available</h3>
+            <p class="text-sm text-[#131313B2] w-3/4 text-center">It is a long established fact that a reader will be distracted by the readable content of a page when looking at 
+                its layout. The point of using Lorem Ipsum is that it has a.</p>
+    `;
+    error.classList.remove("hidden");
+    return;
+  }
+  error.classList.add('hidden');
   for (let pet of pets) {
+    const price = `${pet.price}$`;
     const div = document.createElement("div");
     div.innerHTML = `
         <img src="${pet.image}" class="rounded-md">
@@ -34,16 +48,26 @@ function showPets(pets) {
             <h4 class="text-base font-bold">${pet.pet_name}</h4>
             <div>
                 <p class="flex gap-2 text-sm font-normal text-[#131313B2]">
-                    <img src="../images/breed.svg" class="w-4"> Breed: ${pet.breed}
+                    <img src="../images/breed.svg" class="w-4"> Breed: ${
+                      pet.breed != undefined ? pet.breed : "Unknown"
+                    }
                 </p>
                 <p class="flex gap-2 text-sm font-normal text-[#131313B2]">
-                    <img src="../images/birth.svg" class="w-4"> Birth: ${pet.date_of_birth}
+                    <img src="../images/birth.svg" class="w-4"> Birth: ${
+                      pet.date_of_birth != undefined
+                        ? pet.date_of_birth
+                        : "Unknown"
+                    }
                 </p>
                 <p class="flex gap-2 text-sm font-normal text-[#131313B2]">
-                    <img src="../images/gender.svg" class="w-4"> Gender: ${pet.gender}
+                    <img src="../images/gender.svg" class="w-4"> Gender: ${
+                      pet.gender != undefined ? pet.gender : "Unknown"
+                    }
                 </p>
                 <p class="flex gap-2 text-sm font-normal text-[#131313B2]">
-                    <img src="../images/price.svg" class="w-4"> Price: ${pet.price}$
+                    <img src="../images/price.svg" class="w-4"> Price: ${
+                      pet.price != null ? price : "Unknown"
+                    }
                 </p>
             </div>
         </div>
